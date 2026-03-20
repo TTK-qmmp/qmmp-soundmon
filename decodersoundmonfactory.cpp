9e6842ebfdadb66da0b96533c18bda4a61bcb0bc
Greedysky <greedysky@163.com>
2026-03-20 15:11:00 +0800

    feat: Support for qmmp 2.4

--- decodersoundmonfactory.cpp
+++ decodersoundmonfactory.cpp
@@ -35,19 +35,25 @@ Decoder *DecoderSoundMonFactory::create(const QString &path, QIODevice *input)
     return new DecoderSoundMon(path);
 }
 
-QList<TrackInfo*> DecoderSoundMonFactory::createPlayList(const QString &path, TrackInfo::Parts parts, QStringList *)
+TrackInfoList DecoderSoundMonFactory::createPlayList(const QString &path, TrackInfo::Parts parts, QStringList *)
 {
-    TrackInfo *info = new TrackInfo(path);
+#if QMMP_VERSION_INT < 0x20400
+    TrackInfo *raw(new TrackInfo(path)), *info = raw;
+#else
+    TrackInfo raw(path), info = &raw;
+#endif
     if(parts == TrackInfo::Parts())
     {
-        return QList<TrackInfo*>() << info;
+        return {raw};
     }
 
     SoundMonHelper helper(path);
     if(!helper.initialize())
     {
+#if QMMP_VERSION_INT < 0x20400
         delete info;
-        return QList<TrackInfo*>();
+#endif
+        return {};
     }
 
     if(parts & TrackInfo::Properties)
@@ -59,7 +65,8 @@ QList<TrackInfo*> DecoderSoundMonFactory::createPlayList(const QString &path, Tr
         info->setValue(Qmmp::FORMAT_NAME, "BP SoundMon");
         info->setDuration(helper.totalTime());
     }
-    return QList<TrackInfo*>() << info;
+
+    return {raw};
 }
 
 MetaDataModel* DecoderSoundMonFactory::createMetaDataModel(const QString &path, bool readOnly)
